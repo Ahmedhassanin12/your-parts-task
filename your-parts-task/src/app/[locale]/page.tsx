15f1d17880d10b42d8a360900ef4e668acbdc2f1
ahmed hassanin <ahmedhassanin1891@gmail.com>
2025-02-13 22:27:28 +0200

    change

--- your-parts-task/src/app/[locale]/page.tsx
+++ your-parts-task/src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import HomeModule from "@/modules/Home/HomeModule";
 
 export default function Home() {
 	return (
-		<main className="flex flex-1 w-full bg-slate-100 rounded-lg shadow h-screen overflow-hidden">
+		<main className="flex flex-1 w-full bg-slate-200 rounded-lg shadow h-screen overflow-hidden">
 			<HomeModule />
 		</main>
 	);
